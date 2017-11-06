a1bb44691458e1a04a7aa53ddbe6a30a663cabaf
Jürgen Kappler <jurgen.kappler@liferay.com>
2017-11-06 18:13:05 +0100

    Retrieve all entries for a given group

--- star-wars-demo-web/src/main/java/com/liferay/star/wars/demo/web/internal/portlet/action/StarWarsDemoViewMVCRenderCommand.java
+++ star-wars-demo-web/src/main/java/com/liferay/star/wars/demo/web/internal/portlet/action/StarWarsDemoViewMVCRenderCommand.java
@@ -14,12 +14,26 @@
 
 package com.liferay.star.wars.demo.web.internal.portlet.action;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.template.Template;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.star.wars.demo.model.StarWarsCharacter;
+import com.liferay.star.wars.demo.service.StarWarsCharacterService;
+
+import java.util.List;
 
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Jürgen Kappler
@@ -38,7 +52,46 @@ public class StarWarsDemoViewMVCRenderCommand implements MVCRenderCommand {
 	public String render(
 		RenderRequest renderRequest, RenderResponse renderResponse) {
 
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		try {
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)renderRequest.getAttribute(WebKeys.THEME_DISPLAY);
+
+			List<StarWarsCharacter> starWarsCharacters =
+				_starWarsCharacterService.getStarWarsCharacters(
+					themeDisplay.getScopeGroupId());
+
+			for (StarWarsCharacter starWarsCharacter : starWarsCharacters) {
+				JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+				jsonObject.put(
+					"description", starWarsCharacter.getDescription());
+				jsonObject.put("fraction", starWarsCharacter.getFraction());
+				jsonObject.put("name", starWarsCharacter.getName());
+				jsonObject.put("picture", starWarsCharacter.getPicture());
+
+				jsonArray.put(jsonObject);
+			}
+		}
+		catch (PortalException pe) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(pe);
+			}
+		}
+
+		Template template = (Template)renderRequest.getAttribute(
+			WebKeys.TEMPLATE);
+
+		template.put("starWarsCharacters", jsonArray);
+
 		return "View";
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		StarWarsDemoViewMVCRenderCommand.class);
+
+	@Reference
+	private StarWarsCharacterService _starWarsCharacterService;
 
 }
