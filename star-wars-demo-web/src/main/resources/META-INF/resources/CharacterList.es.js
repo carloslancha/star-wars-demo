5933a11640cd8149632ad94e3d26093c0708767f
Carlos Lancha <carlos.lancha@liferay.com>
2017-11-07 18:49:02 +0100

    Step 9

--- star-wars-demo-web/src/main/resources/META-INF/resources/CharacterList.es.js
+++ star-wars-demo-web/src/main/resources/META-INF/resources/CharacterList.es.js
@@ -1,8 +1,21 @@
 import Component from 'metal-component/src/Component';
 import Soy from 'metal-soy/src/Soy';
 import templates from './CharacterList.soy';
+import {Config} from 'metal-state';
 
-class CharacterList extends Component {}
+class CharacterList extends Component {
+  showCards() {
+		this.displayStyle = 'cards';
+	}
+
+	showList() {
+		this.displayStyle = 'list';
+	}
+}
+
+CharacterList.STATE = {
+  displayStyle: Config.oneOf(['cards', 'list']).value('cards')
+};
 
 // Register component
 Soy.register(CharacterList, templates);
